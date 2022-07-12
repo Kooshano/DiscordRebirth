49a589716b16de512979dd81cee0e69b78bf5a77
Dam0-0n <damoon381@yahoo.com>
2022-07-12 14:54:51 +0430

    Friend list update

--- src/main/java/com/example/demo/FriendController.java
+++ src/main/java/com/example/demo/FriendController.java
@@ -4,10 +4,7 @@ import Client.Client;
 import Client.SavedData;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.ListView;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 
 import static javafx.scene.paint.Color.GREEN;
 import static javafx.scene.paint.Color.RED;
@@ -19,21 +16,29 @@ public class FriendController {
     @FXML
     private Label friendRequestResultLabel;
     @FXML
-    private ListView allFriendListView;
+    private ListView allFriendsListView;
+
 
     public void addFriend(ActionEvent event) throws InterruptedException {
+        if (friendRequestResultLabel.equals("Hm, didn't work. Double check that the capitalization,\n" +
+                "spelling, any spaces, and numbers are correct.")){
+            friendRequestResultLabel.setText("");
+            Thread.sleep(500);
+        }
         SavedData.getClientApp().sendFriendRequest(addFriendText.getText());
         Thread.sleep(500);
         System.out.println(SavedData.getFriendRequestResponse());
         String result = SavedData.getFriendRequestResponse();
-        if (result.equals()){
+        if (result.equals("Hm, didn't work. Double check that the capitalization,\n" +
+                "spelling, any spaces, and numbers are correct.")){
             friendRequestResultLabel.setTextFill(RED);
         }
         else {
             friendRequestResultLabel.setTextFill(GREEN);
+            allFriendsListView.getItems().addAll(SavedData.getFriends());
         }
         friendRequestResultLabel.setText(result);
-        
+
 
     }
 
