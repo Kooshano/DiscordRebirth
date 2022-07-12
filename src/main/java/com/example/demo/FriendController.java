e1ba079ddaeafa9a25a75c9c944b5ec20932582f
Dam0-0n <damoon381@yahoo.com>
2022-07-12 14:32:00 +0430

    Friend list

--- src/main/java/com/example/demo/FriendController.java
+++ src/main/java/com/example/demo/FriendController.java
@@ -6,22 +6,35 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
+
+import static javafx.scene.paint.Color.GREEN;
+import static javafx.scene.paint.Color.RED;
 
 public class FriendController {
 
     @FXML
     private TextField addFriendText;
     @FXML
-    private Button friendRequestButton;
-    @FXML
     private Label friendRequestResultLabel;
+    @FXML
+    private ListView allFriendListView;
 
     public void addFriend(ActionEvent event) throws InterruptedException {
         SavedData.getClientApp().sendFriendRequest(addFriendText.getText());
         Thread.sleep(500);
         System.out.println(SavedData.getFriendRequestResponse());
-        friendRequestResultLabel.setText(SavedData.getFriendRequestResponse());
+        String result = SavedData.getFriendRequestResponse();
+        if (result.equals()){
+            friendRequestResultLabel.setTextFill(RED);
+        }
+        else {
+            friendRequestResultLabel.setTextFill(GREEN);
+        }
+        friendRequestResultLabel.setText(result);
+        
+
     }
 
 
